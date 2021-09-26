8922acf2f8bd20adfe3ac9303dac913385b198be
Jabari Tucker <jabari_tucker@hotmail.com>
2021-09-25 22:41:50 -0600

    Added comments

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,14 @@
+# Load the dplyr package
 library(dplyr)
+
+# Import and read csv file as a data frame
 MechaCar_table <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
+
+# Perform multiple linear regression with mpg as the dependent variable
 lm(mpg ~ vehicle_length +  + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table)
+
+# Determine the p-value and the r-squared value for the linear regression model
 summary(lm(mpg ~ vehicle_length +  + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))
+
+# p-value: 5.35e-11
+# R-squared:  0.6825
