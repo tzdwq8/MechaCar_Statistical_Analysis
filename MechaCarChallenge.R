162b71ff19eca60b98b84d8b2e34db228c7f9837
Jabari Tucker <jabari_tucker@hotmail.com>
2021-09-25 22:26:50 -0600

    Linear regression, mpg as dependent variable

---
+++ MechaCarChallenge.R
@@ -0,0 +1,4 @@
+library(dplyr)
+MechaCar_table <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
+lm(mpg ~ vehicle_length +  + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table)
+summary(lm(mpg ~ vehicle_length +  + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))
