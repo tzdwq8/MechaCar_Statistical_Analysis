2c6a3227f37cc0757929aa1be66e23f1788db8ab
Jabari Tucker <jabari_tucker@hotmail.com>
2021-09-26 19:05:55 -0600

    Completed technical analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,7 +11,7 @@ lm(mpg ~ vehicle_length +  + vehicle_weight + spoiler_angle + ground_clearance +
 summary(lm(mpg ~ vehicle_length +  + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_table))
 
 # p-value: 5.35e-11
-# R-squared:  0.6825
+# R-squared: 0.6825
 
 # Import and read csv file as a data frame
 Suspension_table <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
@@ -19,5 +19,20 @@ Suspension_table <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsA
 # Create data frame to get the mean, median, variance, and standard deviation of the suspension coil's PSI column
 total_summary <- Suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') 
 
-# creates data frame to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI
+# Create data frame to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI
 lot_summary <- Suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI),Variance = var(PSI), SD = sd(PSI))
+
+# Determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+t.test(Suspension_table$PSI, mu = 1500)
+
+# Determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch
+Lot1 = subset(Suspension_table, Manufacturing_Lot == 'Lot1')
+Lot2 = subset(Suspension_table, Manufacturing_Lot == 'Lot2')
+Lot3 = subset(Suspension_table, Manufacturing_Lot == 'Lot3')
+
+t.test(Lot1$PSI, mu = 1500)
+# p-value = 1
+t.test(Lot2$PSI, mu = 1500)
+# p-value = 0.6072
+t.test(Lot3$PSI, mu = 1500)
+# p-value = 0.04168
